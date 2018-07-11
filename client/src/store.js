32ca6ab645fd77091fb94d52063a30f39420cad7
Ryan Smith <rksmith369@gmail.com>
2018-07-10 21:11:19 -0700

    Added/Fixed Usage On FireFoxBrowser

--- client/src/store.js
+++ client/src/store.js
@@ -6,13 +6,23 @@ const initialState = {};
 
 const middleware = [thunk];
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+// Only chrome can handle the redux dev tools,
+//Redux compose cannot handle a null or undefined middleware
+if (window.navigator.userAgent.includes("Chrome")) {
+  var store = createStore(
+    rootReducer,
+    initialState,
+    compose(
+      applyMiddleware(...middleware),
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  );
+} else {
+  var store = createStore(
+    rootReducer,
+    initialState,
+    compose(applyMiddleware(...middleware))
+  );
+}
 
 export default store;
