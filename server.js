097c6d4d08a69d9ceaef74d142c3f38d2acfb2fa
Ryan Smith <rksmith369@gmail.com>
2018-07-16 18:40:11 -0700

    deploy round 5

--- server.js
+++ server.js
@@ -38,13 +38,13 @@ app.use("/api/profile", profile);
 app.use("/api/posts", posts);
 
 // Server Static Asset if in production:  
-// if(process.env.NODE_ENV === "production") {
-//     app.use(express.static("client/build"));
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
 
-//     app.get("*", (req, res) => {
-//         res.sendFile(path.resolve(__direname, "client", "build", "index.html"));
-//     });
-// }
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__direname, "client", "build", "index.html"));
+    });
+}
 
 // Listening
 const port = process.env.PORT || 5000
