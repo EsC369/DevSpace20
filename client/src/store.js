6b428e3c9be884322b8b614051a62b56129d14be
Ryan Smith <rksmith369@gmail.com>
2018-08-02 21:01:36 -0700

    fixed chrome 10

--- client/src/store.js
+++ client/src/store.js
@@ -1,18 +1,31 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers';
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
 
 const initialState = {};
 
 const middleware = [thunk];
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+function BrowserDetection() {
+  // Only chrome can handle the redux dev tools,
+  //Redux compose cannot handle a null or undefined middleware
+  if (navigator.userAgent.search("Chrome")) {
+    var store = createStore(
+      rootReducer,
+      initialState,
+      compose(
+        applyMiddleware(...middleware),
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION__()
+      )
+    );
+  } else {
+    var store = createStore(
+      rootReducer,
+      initialState,
+      compose(applyMiddleware(...middleware))
+    );
+  }
+}
 
 export default store;
