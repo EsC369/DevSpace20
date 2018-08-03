a343aabbf26b719fff582a33c3a1c824946ba441
Ryan Smith <rksmith369@gmail.com>
2018-08-03 09:46:53 -0700

    fix chrome 12

--- client/src/store.js
+++ client/src/store.js
@@ -1,31 +1,18 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
-import rootReducer from "./reducers";
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
 
 const initialState = {};
 
 const middleware = [thunk];
 
-function BrowserDetection() {
-  // Only chrome can handle the redux dev tools,
-  //Redux compose cannot handle a null or undefined middleware
-  if (navigator.userAgent.search("Chrome")) {
-    var store = createStore(
-      rootReducer,
-      initialState,
-      compose(
-        applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ &&
-          window.__REDUX_DEVTOOLS_EXTENSION__()
-      )
-    );
-  } else {
-    var store = createStore(
-      rootReducer,
-      initialState,
-      compose(applyMiddleware(...middleware))
-    );
-  }
-}
+const store = createStore(
+  rootReducer,
+  initialState,
+  compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+  )
+);
 
 export default store;
